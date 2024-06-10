41658b5eb6dba3bbef17726dbb1246defdd2b412
Harshit Sharma <hsharm48@asu.edu>
2024-06-09 23:42:01 -0700

    added kubectl in bash file

--- gateway/install_openfaas_docker.sh
+++ gateway/install_openfaas_docker.sh
@@ -10,8 +10,17 @@ apt-get install -y curl
 # install openfaas cli
 curl -sSL https://cli.openfaas.com | sh
 
-if [ $? -eq 0 ]; then
-    echo "Successfully installed faas-cli"
-else
-    echo "Failed to install faas-cli"
-fi
+# installing 
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+# curl -sSL https://get.arkade.dev | sh
+
+# arkade get faas-cli
+
+# arkade install cron-connector
+
+# install docker
+curl -fsSL https://get.docker.com | sh
+
+
