95fb0d43b166e57776bbd46e84ef3b99bd1ce8d2
Harshit Sharma <hsharm48@asu.edu>
2024-06-17 12:54:55 -0700

    made changes to docker file and install bash script

--- gateway/install_openfaas_docker.sh
+++ gateway/install_openfaas_docker.sh
@@ -16,11 +16,23 @@ chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 
 
-# curl -sSL https://get.arkade.dev | sh
+curl -sSL https://get.arkade.dev | sh
 
-# # arkade get faas-cli
+# arkade get faas-cli
 
-# arkade install cron-connector
+arkade install cron-connector
 
 # install docker
 curl -fsSL https://get.docker.com | sh
+
+# Log in to Docker
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+# generate password for faas-cli
+export PASSWORD=$(head -c 12 /dev/urandom | shasum | cut -d' ' -f1)
+kubectl -n openfaas create secret generic basic-auth \
+  --from-literal=basic-auth-user=admin \
+  --from-literal=basic-auth-password="$PASSWORD"
+
+# Log in to faas-cli
+echo -n $PASSWORD | faas-cli login --username="admin" --password-stdin
