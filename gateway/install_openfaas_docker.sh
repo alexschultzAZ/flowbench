e4c79994b036c37b4480906b45c44f5a2474a4a7
Harshit Sharma <hsharm48@asu.edu>
2024-06-17 12:25:44 -0700

    added pvc and sc files for nfs storage

--- gateway/install_openfaas_docker.sh
+++ gateway/install_openfaas_docker.sh
@@ -14,6 +14,8 @@ curl -sSL https://cli.openfaas.com | sh
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
+
+
 # curl -sSL https://get.arkade.dev | sh
 
 # arkade get faas-cli
